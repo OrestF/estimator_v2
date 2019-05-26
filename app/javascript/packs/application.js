b68b5ce7870415499313e891c4bcc6cd3caf7a14
OrestF <falchuko@gmail.com>
2019-05-26 16:36:25 +0300

    Init project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,12 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+require("channels");
+import 'bootstrap/dist/js/bootstrap';
 
-
+$(function () {
+    console.log('Hello World from Webpacker');
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
