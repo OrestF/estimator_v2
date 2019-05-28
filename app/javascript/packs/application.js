cd8e404008fb9b54de798893423f4d92387e608e
OrestF <falchuko@gmail.com>
2019-05-28 21:12:53 +0300

    Set correct assets for webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
 require("channels");
+import '../stylesheets/application';
+
 import 'bootstrap/dist/js/bootstrap';
+import 'daemonite-material/js/material';
+
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
