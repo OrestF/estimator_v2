b85ec68a5e47e9c124341ff6d848d85f7166ef67
Orest <orest@Orests-iMac-2.local>
2019-10-16 15:04:07 +0300

    Update UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   def nav_logo
     org_logo(size: '32x32').presence || image_tag('estimator', size: '32x32')
   end
+
+  def corner_button(url, **args)
+    content_tag(:button, class: "btn btn-float btn-primary corner-button #{args[:css]}") do
+      icon_link(url, **args)
+    end
+  end
+
+  def icon_link(url, **args)
+    link_to(url, **args[:link_params].to_h) do
+      content_tag(:i, args[:icon] || 'link', class: 'material-icons', title: args[:title].presence || args[:icon].to_s.humanize)
+    end
+  end
+
+  def corner_dropdown(**args)
+    ApplicationController.new.render_to_string(partial: 'ui/corner_dropdown', assigns: args)
+  end
 end
