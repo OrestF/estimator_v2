8b83a128f1bcf97569437dfa2f8f715ae0a89796
Orest <orest@Orests-iMac-2.local>
2019-11-18 12:40:07 +0200

    Refactored scopes and namespaces

---
+++ app/controllers/concerns/specifications/features.rb
@@ -0,0 +1,47 @@
+module Specifications::Features
+  extend ActiveSupport::Concern
+
+  def create_feature
+    respond_to do |format|
+      if (res = Features::Operations::Create.call(record_params: feature_params)).success?
+        format.json { render_feature_created(res.data[:record]) }
+        format.js   { render_feature_created(res.data[:record]) }
+      else
+        format.json { error_nf(html_humanize_errors(res.errors)) }
+        format.js   { error_nf(html_humanize_errors(res.errors)) }
+      end
+    end
+  end
+
+  def update_feature
+    respond_to do |format|
+      if (res = Features::Operations::Update.call(record: feature, record_params: feature_params)).success?
+        format.json { render_feature_updated(res.data[:record]) }
+        format.js   { render_feature_updated(res.data[:record]) }
+      else
+        format.json { error_nf(html_humanize_errors(res.errors)) }
+        format.js   { error_nf(html_humanize_errors(res.errors)) }
+      end
+    end
+  end
+
+  private
+
+  def feature
+    Feature.find(feature_params[:id])
+  end
+
+  def feature_params
+    params.require(:feature).permit!.to_h.merge!(user_id: current_user.id)
+  end
+
+  def render_feature_created(feature, _message: nil)
+    render 'specifications/features/created', format: :js, status: :ok, locals: { feature: feature,
+                                                                                  specification: record }
+  end
+
+  def render_feature_updated(feature, _message: nil)
+    render 'specifications/features/updated', format: :js, status: :ok, locals: { feature: feature,
+                                                                                  specification: record }
+  end
+end
--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -1,4 +1,6 @@
 class EstimationsController < ResourcesController
+  include Estimations::EstimationTasks
+
   def index
     respond_to do |format|
       format.html
@@ -9,30 +11,6 @@ class EstimationsController < ResourcesController
   def show; end
 
   def edit; end
-
-  def create_task
-    respond_to do |format|
-      if (res = EstimationTasks::Operations::Create.call(record_params: estimation_task_params)).success?
-        format.json { render_created(res.data[:record]) }
-        format.js   { render_created(res.data[:record]) }
-      else
-        format.json { error_nf(html_humanize_errors(res.errors)) }
-        format.js   { error_nf(html_humanize_errors(res.errors)) }
-      end
-    end
-  end
-
-  def update_task
-    respond_to do |format|
-      if (res = EstimationTasks::Operations::Update.call(record: estimation_task, record_params: estimation_task_params)).success?
-        format.json { render_updated(res.data[:record]) }
-        format.js   { render_updated(res.data[:record]) }
-      else
-        format.json { error_nf(html_humanize_errors(res.errors)) }
-        format.js   { error_nf(html_humanize_errors(res.errors)) }
-      end
-    end
-  end
 
   def done
     if (res = Estimations::Operations::Done.call(record: record)).success?
@@ -46,25 +24,5 @@ class EstimationsController < ResourcesController
 
   def record_class
     Estimation
-  end
-
-  def estimation_task_params
-    params.require(:estimation_task).permit!
-  end
-
-  def estimation_task
-    EstimationTask.find(estimation_task_params[:id])
-  end
-
-  def render_created(e_task, _message: nil)
-    render 'estimations/task_created', format: :js, status: :ok, locals: { estimation_task: e_task,
-                                                                           estimation: record,
-                                                                           totals: e_task.estimation.totals }
-  end
-
-  def render_updated(e_task, _message: nil)
-    render 'estimations/task_updated', format: :js, status: :ok, locals: { estimation_task: e_task,
-                                                                           estimation: record,
-                                                                           totals: e_task.estimation.totals }
   end
 end
