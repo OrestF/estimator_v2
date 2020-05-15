060e71585a24cdca27072807f8f24e580fe4b693
Orest <orest@Orests-iMac-2.local>
2020-05-15 15:22:50 +0300

    Fix rcreds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  routes.default_url_options = { host: RCreds(:network, :host) }
+  routes.default_url_options = { host: RCreds.fetch(:network, :host) }
   config.action_mailer.default_url_options = routes.default_url_options
   config.action_mailer.delivery_method = :mailjet
 end
